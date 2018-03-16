a24ad5c0b8ef7ecb75fb68cae18185f48b91f3e1
HL56 <512663808@qq.com>
2018-03-16 19:32:10 +0800

    Add files via upload

--- 2.js
+++ 2.js
@@ -19,7 +19,7 @@
    }
 
     function fun1() {
-        var name = "https://wenku.baidu.com/view/a9048508492fb4daa58da0116c175f0e7cd119d8.html";
+        var name = "https://wenku.baidu.com/view/be81c2cc9fc3d5bbfd0a79563c1ec5da50e2d6df.html";
         document.getElementById("test").setAttribute("href",name);
         document.getElementById("test").innerHTML = "四年级点我";
         document.getElementById("test").style.color = "springgreen";
